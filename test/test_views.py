5be475e3a360b4525916f7954dbb45ac157c1073
student-wroclaw <student-wroclaw@EDU-WSB.local>
2022-11-13 17:06:25 +0100

    ok

--- test/test_views.py
+++ test/test_views.py
@@ -13,6 +13,7 @@ class FlaskrTestCase(unittest.TestCase):
         s = str(rv.data)
         ','.join(SUPPORTED) in s
 
+xd
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
         self.assertEqual(b'{ "imie":"Mateusz", "msg":"Hello World!"}', rv.data)
